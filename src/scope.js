baf3c40b41d2426aae4501719b9f36957d39a3f2
Soeren Pedersen <speconsult@gmail.com>
2014-06-29 21:11:50 +0200

    scope

--- src/scope.js
+++ src/scope.js
@@ -6,6 +6,7 @@ var Scope = function () {
     this.$$asyncQueue = [];
     this.$$postDigestQueue = [];
     this.$$phase = null;
+    this.$$children = [];
 
 };
 
@@ -42,34 +43,36 @@ Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
 //loop through the watchers once
 Scope.prototype.$$digestOnce = function () {
 
+    var dirty;
+    var continueLoop = true;
     var self = this;
-    var newValue, oldValue, dirty;
+    this.$$everyScope(function (scope) {
+        var newValue, oldValue;
+        //we use the ECMA script 5 method on Array - every(). It breaks the loop when falsy is returned
+        _.forEachRight(scope.$$watchers, function (watcher) {
 
-    //we use the ECMA script 5 method on Array - every(). It breaks the loop when falsy is returned
-    _.forEachRight(this.$$watchers, function (watcher) {
+            try {
+                if (watcher) {
+                    newValue = watcher.watchFn(scope);
+                    oldValue = watcher.last;
 
-        try {
-            if (watcher) {
-                newValue = watcher.watchFn(self);
-                oldValue = watcher.last;
+                    if (!scope.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+                        self.$$lastDirtyWatch = watcher;
+                        watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+                        watcher.listenerFn(newValue,
+                            (oldValue === initWatchVal ? newValue : oldValue), scope);
+                        dirty = true;
 
-                if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-                    self.$$lastDirtyWatch = watcher;
-                    watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-                    watcher.listenerFn(newValue,
-                        (oldValue === initWatchVal ? newValue : oldValue), self);
-                    dirty = true;
-
-                } else if (self.$$lastDirtyWatch === watcher) {
-                    //break the loop
-                    return false;
+                    } else if (self.$$lastDirtyWatch === watcher) {
+                        continueLoop = false;
+                        return false;
+                    }
                 }
+            } catch (e) {
+                console.error(e);
             }
-        } catch (e) {
-            console.error(e);
-        }
-        //stay in the loop
-        return true;
+        });
+        return continueLoop;
     });
     return dirty;
 };
@@ -175,6 +178,18 @@ Scope.prototype.$new = function () {
     var ChildScope = function () {};
     ChildScope.prototype = this;
     var child = new ChildScope();
+    this.$$children.push(child);
     child.$$watchers = [];
+    child.$$children = [];
     return child;
-}
+};
+
+Scope.prototype.$$everyScope = function(fn) {
+    if (fn(this)) {
+        return this.$$children.every(function (child) {
+            return child.$$everyScope(fn);
+        });
+    } else {
+        return false;
+    }
+};
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -720,7 +720,7 @@ describe('Scope', function () {
                     return scope.aValue;
                 },
                 function (newValue, oldValue, scope) {
-                    scope.counter ++;
+                    scope.counter++;
                 },
                 true
             );
@@ -772,7 +772,7 @@ describe('Scope', function () {
         it('does not shadow members of parent scopes attributes', function () {
             var parent = new Scope();
             var child = parent.$new();
-            
+
             parent.user = {user: 'joe'};
             child.user.name = 'jill';
 
@@ -799,6 +799,57 @@ describe('Scope', function () {
             child.$digest();
             expect(child.aValueWas).toBeUndefined();
 
+        });
+
+        it('keeps a record of its childre', function () {
+            var parent = new Scope();
+            var child1 = parent.$new();
+            var child2 = parent.$new();
+            var child2_1 = parent.$new();
+
+            expect(parent.$$children.length).toBe(2);
+            expect(parent.$$children[0]).toBe(child1);
+            expect(parent.$$children[1]).toBe(child2);
+
+            expect(child1.$$children.length).toBe(0);
+            expect(child2.$$children.length).toBe(1);
+            expect(child2.$$children[0]).toBe(child2_1);
+
+        });
+
+        it('digests its children', function () {
+            var parent = new Scope();
+            var child = parent.$new();
+
+            parent.aValue = 'abc';
+            child.$watch(function (scope) {
+                    return scope.aValue;
+                }, function (newValue, oldValue, scope) {
+                    scope.aValueWas = newValue;
+                }
+            );
+
+            parent.$digest();
+            expect(child.aValueWas).toBe('abc');
+        });
+
+        it('digests from root on $apply', function () {
+            var parent = new Scope();
+            var child = parent.$new();
+            var child2 = child.$new();
+
+            parent.aValue = 'abc';
+            parent.counter = 0;
+
+            parent.$watch(function (scope) {
+                return scope.aValue;
+            }, function (newValue, oldValue, scope) {
+                scope.counter ++;
+            })
+
+            child2.$apply(function (scope) {})
+
+            expect(parent.counter).toBe(1);
         });
     });
 
