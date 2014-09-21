b7e6a832700ac0dc2754c17539fec5b6fe0236a6
zapp <obe2lix>
2014-09-21 18:51:11 +0200

    small fix

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1083,6 +1083,28 @@ describe("Scope", function () {
             expect(scope.counter).toBe(2);
 
         });
+
+        it("works like a normal watch for NaNs", function () {
+
+            scope.aValue = 0 / 0;
+            scope.counter = 0;
+
+            scope.$watch(
+                function (scope) {
+                    return scope.aValue;
+                },
+                function (newValue, oldValue, scope) {
+                    scope.counter ++;
+                }
+            );
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+
+        })
+
     });
 
 });
