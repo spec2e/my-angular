d3fdce35757122cfac9021bf088027ead00899a3
zapp <obe2lix>
2014-09-21 21:14:06 +0200

    small fix

--- src/scope.js
+++ src/scope.js
@@ -51,10 +51,35 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
     var internalWatchFn = function (scope) {
         newValue = watchFn(scope);
 
-        if(!self.$$areEqual(newValue, oldValue, false)) {
-            changeCount ++;
+        if (_.isObject(newValue)) {
+            if(_.isArray(newValue)) {
+                if(!_.isArray(oldValue)) {
+                    changeCount ++;
+                    oldValue = [];
+                }
+
+                if(newValue.length !== oldValue.length) {
+                    changeCount ++;
+                    oldValue.length = newValue.length;
+                }
+
+                _.forEach(newValue, function (newItem, i) {
+                    var bothNaN = _.isNaN(newItem) && _.isNaN(oldValue[i]);
+                    if(!bothNaN && newItem !== oldValue[i]) {
+                        changeCount ++;
+                        oldValue[i] = newItem;
+                    }
+                })
+
+            } else {
+
+            }
+        } else {
+            if (!self.$$areEqual(newValue, oldValue, false)) {
+                changeCount++;
+            }
+            oldValue = newValue;
         }
-        oldValue = newValue;
 
         return changeCount;
     };
